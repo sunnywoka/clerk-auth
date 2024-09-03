bb8b685169c5794bdcc7a64e7c3aafec8397da04
Yuekai Wu <yuekaixx@outlook.com>
2024-09-03 21:57:42 +1200

    set up clerk

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
+
+export default clerkMiddleware()
